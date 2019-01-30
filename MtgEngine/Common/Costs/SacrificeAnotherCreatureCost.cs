bbd1fe322a71cfe912ca7e94b58030f75f02c45d
Frederick Arthur Hallock III <faha223@uky.edu>
2019-01-30 17:32:27 -0500

    Added SacrificeAnotherCreatureCost for cards like Ravenous Harpy

---
+++ MtgEngine/Common/Costs/SacrificeAnotherCreatureCost.cs
@@ -0,0 +1,9 @@
+﻿namespace MtgEngine.Common.Costs
+{
+    public class SacrificeAnotherCreatureCost : SacrificeTargetCost
+    {
+        public SacrificeAnotherCreatureCost(IResolvable source) : base(source, card => card != source && card.IsACreature)
+        {
+        }
+    }
+}
