47a101e0b53348b7b0d2d6bac77fae17b3f13d21
Frederick Arthur Hallock III <faha223@uky.edu>
2019-05-09 16:14:08 -0400

    Moved Counterspell into Alpha, changed the TestSet version to just inherit from Alpha

---
+++ MtgEngine.Alpha/Instants/Counterspell.cs
@@ -0,0 +1,45 @@
+﻿using MtgEngine.Common;
+using MtgEngine.Common.Cards;
+using MtgEngine.Common.Costs;
+using MtgEngine.Common.Enums;
+using MtgEngine.Common.Players;
+using System;
+using System.Collections.Generic;
+
+namespace MtgEngine.Alpha.Instants
+{
+    [MtgCard("Counterspell", "LEA", "", "", "Counter target spell")]
+    public class Counterspell : SpellCard
+    {
+        Card target;
+
+        public Counterspell(Player owner) :
+            base(owner, null, new[] { CardType.Instant }, null, false)
+        {
+            Cost = ManaCost.Parse(this, "{U}{U}");
+        }
+
+        public override bool CanCast(Game game)
+        {
+            // Can't cast if there are no targets
+            var possibleTargets = game.CardsOnStack();
+            if (possibleTargets.Count == 0)
+                return false;
+
+            return base.CanCast(game);
+        }
+
+        public override void OnCast(Game game)
+        {
+            var possibleTargets = game.CardsOnStack();
+            if (possibleTargets.Count == 0)
+                throw new InvalidOperationException("Counterspell can't be cast if there are no spells on the stack");
+            target = Controller.ChooseTarget(this, new List<ITarget>(possibleTargets)) as Card;
+        }
+
+        public override void OnResolve(Game game)
+        {
+            game.Counter(target);
+        }
+    }
+}
--- MtgEngine.TestSet/Instants/Counterspell.cs
+++ MtgEngine.TestSet/Instants/Counterspell.cs
@@ -1,45 +1,11 @@
-﻿using MtgEngine.Common;
-using MtgEngine.Common.Cards;
-using MtgEngine.Common.Costs;
-using MtgEngine.Common.Enums;
+﻿using MtgEngine.Common.Cards;
 using MtgEngine.Common.Players;
-using System;
-using System.Collections.Generic;
 
 namespace MtgEngine.TestSet
 {
     [MtgCard("Counterspell", "TestSet", "", "", "Counter target spell")]
-    public class Counterspell : SpellCard
+    public class Counterspell : Alpha.Instants.Counterspell
     {
-        Card target;
-
-        public Counterspell(Player owner) : 
-            base(owner, null, new[] { CardType.Instant }, null, false)
-        {
-            Cost = ManaCost.Parse(this, "{U}{U}");
-        }
-
-        public override bool CanCast(Game game)
-        {
-            // Can't cast if there are no targets
-            var possibleTargets = game.CardsOnStack();
-            if (possibleTargets.Count == 0)
-                return false;
-
-            return base.CanCast(game);
-        }
-
-        public override void OnCast(Game game)
-        {
-            var possibleTargets = game.CardsOnStack();
-            if (possibleTargets.Count == 0)
-                throw new InvalidOperationException("Counterspell can't be cast if there are no spells on the stack");
-            target = Controller.ChooseTarget(this, new List<ITarget>(possibleTargets)) as Card;
-        }
-
-        public override void OnResolve(Game game)
-        {
-            game.Counter(target);
-        }
+        public Counterspell(Player owner) : base(owner) { }
     }
 }
